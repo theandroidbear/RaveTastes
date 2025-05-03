5f9a99188d896de839e3a7cdbd7e6210dc8e99fc
Moses_Ndungu_G_ <moisendungu@gmail.com>
2025-05-03 14:28:37 +0300

    Added pager to FeedFragment.java

---
+++ app/src/main/java/com/ravemaster/recipeapp/activities/PagerActivityTesting.java
@@ -0,0 +1,26 @@
+package com.ravemaster.recipeapp.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ravemaster.recipeapp.databinding.ActivityPagerTestingBinding;
+
+public class PagerActivityTesting extends AppCompatActivity {
+
+    ActivityPagerTestingBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityPagerTestingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
